2dbc838567a93a976de2ba1e832988735c77dd34
the-anmol <ainsa2279@gmail.com>
2022-12-13 22:24:56 +0530

    remove : unwanted js script

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,55 +1,7 @@
 (function ($) {
-  "use strict";
+  ("use strict");
 
-  /**Accordion js**/
   $(document).ready(function ($) {
-    $(".accordion-toggle").on("click", function (event) {
-      event.preventDefault();
-      var accordion = $(this);
-      var accordionContent = accordion.next(".accordion-content");
-      var accordionToggleIcon = $(this).children(".toggle-icon");
-      accordion.toggleClass("open");
-      accordionContent.slideToggle(250);
-      if (accordion.hasClass("open")) {
-        accordionToggleIcon.html("<i class='fa fa-minus-circle'></i>");
-      } else {
-        accordionToggleIcon.html("<i class='fa fa-plus-circle'></i>");
-      }
-    });
-
-    /** Owl Carousel Js**/
-    $(".discover-slide").owlCarousel({
-      items: 1,
-      nav: true,
-      dots: false,
-      autoplay: false,
-      loop: true,
-      navText: [
-        "<i class='fa fa-long-arrow-left'></i>",
-        "<i class='fa fa-long-arrow-right'></i>",
-      ],
-      mouseDrag: false,
-      touchDrag: false,
-    });
-
-    /** MagnificPopup **/
-    $(".single-project-item a.project-priview").magnificPopup({
-      type: "image",
-      gallery: {
-        enabled: true,
-      },
-    });
-
-    $(".video-bg a").magnificPopup({
-      type: "iframe",
-      gallery: {
-        enabled: true,
-      },
-    });
-
-    /** MixItUp JS**/
-    $("#project-wrapper").mixItUp();
-
     /** Counter Js **/
     $(".counter").counterUp({
       delay: 10,
